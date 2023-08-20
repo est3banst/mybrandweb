f5c1bfaa374e070f89cff7413daa4339c6e524cb
est3banst <estebeeen@outlook.com>
2023-08-19 21:59:09 -0300

    modified some js

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const modal = document.getElementById('about-us');
+const usbut = document.getElementById('us-button');
+
+const navmenu = document.querySelector('.nav-menu');
+
 
 function Toggle() {
     modal.style.position = 'fixed';
-    modal.style.left = '10%'
-    modal.style.top = '15%'
-    modal.style.zIndex = '500'
-    modal.style.fontSize = '1.2rem'
-    modal.hidden == true ? modal.hidden = false : modal.hidden = true
+    modal.style.left = '10%';
+    modal.style.top = '15%';
+    modal.style.zIndex = '500';
+    
+    usbut.style.opacity = '.3'
+    
+    modal.hidden == true ? modal.hidden = false : modal.hidden = true;
+    
 };
+function exitModal() {
+    modal.hidden = true;
+    usbut.style.opacity = '1'
+}
+
+
+
