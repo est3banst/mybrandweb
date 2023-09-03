1207e4d593d9cfcf0f1144788b802a157b74ecb9
est3banst <estebeeen@outlook.com>
2023-09-03 20:23:38 -0300

    fixed mob-menu

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-const modal = document.getElementById('about-us');
-const usbut = document.getElementById('us-button');
-const mobmenu = document.querySelector('.menu-mobile-container')
-const navmenu = document.querySelector('.nav-menu');
+const mobmenu = document.querySelector('.responsive-container-menu')
+const iconmenu = document.querySelector('.icon-container')
+const navmenu = document.querySelector('.menu-a-container');
 
 
 function Toggle() {
-    modal.style.position = 'fixed';
-    modal.style.left = '10%';
-    modal.style.top = '15%';
-    modal.style.zIndex = '500';
-    
-    usbut.style.opacity = '.3';
-    
-    modal.hidden == true ? modal.hidden = false : modal.hidden = true;
-    
+    navmenu.hidden == true ? navmenu.hidden = false : navmenu.hidden = true;
+    navmenu.style.color = "black";
+    navmenu.style.zIndex = '600';
+    navmenu.style.position = 'fixed';
+    navmenu.style.backgroundColor = 'antiquewhite';
+    navmenu.style.height = '80vh'
+    navmenu.style.width = '30%'
 };
-function exitModal() {
-    modal.hidden = true;
-    usbut.style.opacity = '1'
-}
 
 
 
