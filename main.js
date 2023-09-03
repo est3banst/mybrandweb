bf894796792f0a6c95e7e4cdbed500d1ee6aa6be
est3banst <estebeeen@outlook.com>
2023-09-03 20:56:03 -0300

    fixed more th

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ const mobmenu = document.querySelector('.responsive-container-menu')
 const iconmenu = document.querySelector('.icon-container')
 const navmenu = document.querySelector('.menu-a-container');
 
-
-function Toggle() {
-    navmenu.hidden == true ? navmenu.hidden = false : navmenu.hidden = true;
+function setStyle() {
     navmenu.style.color = "black";
     navmenu.style.zIndex = '600';
     navmenu.style.position = 'fixed';
+    iconmenu.style.position = 'fixed';
     navmenu.style.backgroundColor = 'antiquewhite';
     navmenu.style.height = '80vh'
     navmenu.style.width = '30%'
+    navmenu.style.top = '5%'
+    navmenu.style.left = '0'
+}
+
+function Toggle() {
+    navmenu.hidden == true ? navmenu.hidden = false : navmenu.hidden = true;
+    setStyle();
 };
 
 
